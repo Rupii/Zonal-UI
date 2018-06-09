3544555c7e2444c90655a4709d08a88eea3017d8
rupii <mudavathrupesh@gmail.com>
2018-06-09 21:56:46 +0530

    registration removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './components/registration/registration.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { EngineerComponent } from './components/engineer/engineer.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegistrationComponent,
-    CustomerComponent,
-    EngineerComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CustomerComponent, EngineerComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
