6d385ee17e1c4475ad15bc18d75bc931c997b161
rupii <mudavathrupesh@gmail.com>
2018-06-09 20:13:32 +0530

    customer and engineer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { EngineerComponent } from './components/engineer/engineer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    CustomerComponent,
+    EngineerComponent
   ],
   imports: [
     BrowserModule
